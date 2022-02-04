6c6c8b4abe80509dea2425cbb82f6217a28a3f1a
seilys <seilysir@gmail.com>
2022-02-04 15:10:24 +0100

    eliminamos comentarios de freelancer_rates. pasamos variables a snake_case

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,25 +1,20 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
     8.0 * hourly_rate
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
     before_discount * (1 - discount / 100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
-    dailyRate = FreelancerRates.daily_rate(hourly_rate)
-    appliedDiscount = FreelancerRates.apply_discount(22 * dailyRate, discount)
-    trunc(Float.ceil(appliedDiscount))
+    applied_discount = apply_discount(22 * daily_rate(hourly_rate), discount)
+    trunc(Float.ceil(applied_discount))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
-    dailyPriceWithDiscount = apply_discount(daily_rate(hourly_rate), discount)
-    finalPrice = budget / dailyPriceWithDiscount
-    Float.floor(finalPrice, 1)
+    daily_price_with_discount = apply_discount(daily_rate(hourly_rate), discount)
+    final_price = budget / daily_price_with_discount
+    Float.floor(final_price, 1)
   end
 end
